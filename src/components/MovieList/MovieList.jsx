550c4d44162d2e94c36751153ece45d71ab584d5
Remix007 <forvard16back@gmail.com>
2023-03-08 15:28:43 +0200

    List

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,42 @@
+import { MovieLink, MovieLinkBox, Image, Thumb, ShadowBox} from "./MovieList.styled";
+import { useLocation } from "react-router-dom";
+import Placeholder from "../../placeholders/placeholder_for_movie.jpg";
+import PropTypes from "prop-types";
+
+export const MovieList = ({data}) => {
+const location = useLocation()
+const pathVar = location.pathname === "/" ? "/movies/" : "";
+
+    if(!data) return
+    return <><MovieLinkBox>
+        {data.results.map(({id, poster_path, title, release_date}) => {
+        const year = release_date ? release_date.slice(0, 4): null;
+        return <ShadowBox key = {id}><MovieLink 
+        to={`${pathVar}${id}`} 
+        state ={{from: location}}>
+            <Image 
+            src = {"https://image.tmdb.org/t/p/w500/" + (poster_path ? poster_path : Placeholder)} 
+            alt="movie poster" 
+            width="270" 
+            height="405" 
+            onError={({ currentTarget }) => {
+        currentTarget.onerror = null;
+        currentTarget.src=Placeholder;
+  }}/>
+            <Thumb><span>{title}</span><span>{year}</span></Thumb></MovieLink></ShadowBox>}
+        )}
+    </MovieLinkBox>
+    </>
+}
+
+MovieList.propTypes = {
+    data: PropTypes.shape({
+        results: PropTypes.arrayOf( PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            poster_path: PropTypes.string,
+            title: PropTypes.string.isRequired,
+            release_date: PropTypes.string,
+        }).isRequired
+        ).isRequired
+    })
+}
---
+++ src/components/MovieList/MovieList.styled.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const MovieLink = styled(Link)`
+display: flex;
+flex-direction: column;
+width: 270px;
+text-decoration: none;
+color: black;
+font-weight: 500;
+background-color: lightgrey;
+height: 100%;
+&:not(:last-child) {
+margin-bottom: 15px;
+}
+`
+export const ShadowBox = styled.li`
+box-shadow: 3px 3px 3px black;
+`
+export const MovieLinkBox = styled.ul`
+padding: 20px 0;
+display: flex;
+gap: 40px;
+flex-wrap: wrap;
+flex-basis: calc((100% - (3 * 40 )) / 4);
+`
+export const Image = styled.img`
+display: block;
+`
+export const Thumb = styled.div`
+padding: 10px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-grow: 1;
+`
