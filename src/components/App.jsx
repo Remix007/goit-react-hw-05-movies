e221db2ba12178678299c60038835b8506df43a5
Remix007 <forvard16back@gmail.com>
2023-03-08 15:13:48 +0200

    appBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import { Navigate, Route, Routes } from "react-router-dom";
+import { lazy} from "react";
+import Layout from "../pages/Layout";
+
+const Movies = lazy(() => import("../pages/Movies"));
+const Home = lazy(() => import("pages/Home") );
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+const Cast = lazy (() => import("./Cast/Cast"));
+const Reviews = lazy (() => import("./Reviews/Reviews"));
+
+export const App =  () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+<Routes>
+  <Route path="/" element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path="movies" element={<Movies/>}/>
+      <Route path="movies/:movieId" element={<MovieDetails/>}>
+        <Route path="cast" element={<Cast/>}/>
+        <Route path="reviews" element={<Reviews/>}/>
+      </Route>
+      <Route path="*" element={<Navigate to="/"/>}></Route>
+  </Route>
+</Routes>
+</div>
   );
 };
---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,14 @@
+import { Header, NavItem, Nav } from "./AppBar.styled";
+
+export const AppBar = () => {
+return (
+    <>
+    <Header>
+        <Nav>
+            <NavItem to="/" end>Home</NavItem>
+            <NavItem to="movies">Movies</NavItem>
+        </Nav>
+    </Header>
+        </>
+)
+}
